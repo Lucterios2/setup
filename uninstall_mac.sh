d91a581b412523fc33405eb8625751003397d8ed
Laurent GAY <l.gay@sd-libre.fr>
2016-04-02 21:31:38 +0200

    sign windows

--- uninstall_mac.sh
+++ uninstall_mac.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$(id -u)" != "0" ]; then
-   echo ">>> This script must be run as 'super user' <<<" 1>&2
-   [ -z "$(which sudo)" ] && exit 1
-   sudo -E $0 $@
-   exit $!
-fi
-
 if [ -z "$APP_NAME" ]
 then
 	APP_NAME="@@NAME@@"
@@ -16,7 +9,7 @@ echo "====== delete lucterios #@@BUILD@@ ======"
 rm -f /usr/local/bin/launch_lucterios
 rm -f /usr/local/bin/launch_lucterios_gui
 rm -rf $HOME/lucterios2
-rm -rf /var/lucterios2/
+[ -d "/var/lucterios2/" ]  && sudo rm -rf /var/lucterios2/
 rm -rf /usr/share/applications/lucterios.desktop
 rm -rf /Applications/$APP_NAME.*
 echo "============ END ============="
